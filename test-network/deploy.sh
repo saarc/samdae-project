36fe6b4d7605fe5f6cb44f2731a09518189cd7a3
saarc <saarc99@naver.com>
2024-11-07 21:06:54 +0900

    Update deploy.sh

--- test-network/deploy.sh
+++ test-network/deploy.sh
@@ -4,6 +4,6 @@ export FABRIC_CFG_PATH=~/fabric-samples/config
 
 pushd ~/fabric-samples/test-network
 
-    ./network.sh deployCC -ccn utxo -ccp ~/dev/samdae-project/contract/utxo-token -ccl go -c donachannel
+    ./network.sh deployCC -ccn samdae -ccp ~/dev/samdae-project/contract/samdae -ccl go -c supplynet
 
 popd
